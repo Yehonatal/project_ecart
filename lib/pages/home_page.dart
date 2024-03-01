9dbbc075b097a9bb34d82c26e82b58dfc54fbf9c
yonatan afewerk <yonatanafewerk@gmail.com>
2024-03-01 18:57:41 +0300

    Dev: Intro page made, google nav bar added to the home page :)

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,21 @@
+import "package:flutter/material.dart";
+import "package:project_ecart/components/bottom_nav_bar.dart";
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      bottomNavigationBar: BottomNavBar(
+        onTabChange: (index) => {},
+      ),
+    );
+  }
+}
---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,83 @@
+import "package:flutter/material.dart";
+import "package:project_ecart/pages/home_page.dart";
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[300],
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Logo
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Image.asset(
+                    'assets/images/nike.png',
+                    height: 240,
+                  ),
+                ),
+                const SizedBox(
+                  height: 45,
+                ),
+
+                // Title
+                const Text(
+                  "Just Do It",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 28,
+                  ),
+                ),
+
+                // Sub title
+                const Padding(
+                  padding: EdgeInsets.all(25),
+                  child: Text(
+                    "Brand new sneakers and custom kicks made with premium quality.",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                const SizedBox(
+                  height: 45,
+                ),
+                // start button
+
+                GestureDetector(
+                  onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const HomePage())),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(8)),
+                    child: const Center(
+                      child: Padding(
+                        padding: EdgeInsets.all(18),
+                        child: Text(
+                          "Shop Now",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w800,
+                              fontSize: 16),
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
